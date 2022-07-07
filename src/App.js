9350ccc07cc527eedfb40551e8cce654ec0c1bdc
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-06 20:41:59 -0400

    evaluate function

--- src/App.js
+++ src/App.js
@@ -41,11 +41,42 @@ function reducer(state, { type, payload }) {
           currentOperand: null,
         };
       }
-      break;
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
     case ACTIONS.CLEAR:
       break;
     default:
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "÷":
+      computation = prev / current;
+      break;
+    default:
+  }
+
+  return computation.toString;
 }
 
 function App() {
